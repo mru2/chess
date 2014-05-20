0209d37b2f3c6ad7774b8853f8b522f1c27b25eb
David Ruyer <david.ruyer@clicrdv.com>
2014-05-20 15:17:30 +0200

    js cleanup

--- js/main.js
+++ js/main.js
@@ -1,86 +1,54 @@
 angular.module("MyApp", ["firebase"])
-.service('Game', function($firebase, $rootScope){
+.service('Game', function($firebase, $q, $rootScope){
 
-  var defaultFen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+  // var defaultFen = 
 
-  var gameRef = new Firebase("https://torid-fire-3930.firebaseio.com/game");
-  var fen = $firebase(gameRef);
-  var game = null;
-  var ready = false;
+  var remoteDB = $firebase(new Firebase("https://torid-fire-3930.firebaseio.com/game"));
 
-  fen.$on('loaded', function(){
-    $rootScope.$apply(function(){
-      game = new Chess(fen.$value);
-      ready = true;
-    });
+  var deferred = $q.defer();
 
-    fen.$on('change', function(){
-      console.log('update!');
+  // Return a promise for the game
+  remoteDB.$on('loaded', function(){
+
+    var fen   = remoteDB.$value;
+    var game  = new Chess(fen);
+
+    // Custom methods on the game object
+    game.save = function(){
+      console.log('persisting game', this.fen());
+      remoteDB.$set(this.fen());
+    };
+
+    game.clear = function(){
+      this.load('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1');
+      this.save();
+    };
+
+    // Watching remote DB changes
+    remoteDB.$on('change', function(){
       $rootScope.$apply(function(){
-        game.load(fen.$value);
+        game.load(remoteDB.$value);
       });
     });
+
+    deferred.resolve(game);
+
   });
 
-
-  return {
-    ready: function(){
-      return ready;
-    },
-
-    fen: function(){
-      return game.fen();
-    },
-
-    update: function(newFen){
-      if(newFen !== this.fen()){
-        console.log('updating remote game to', newFen);
-        fen.$set(newFen);
-        game.load(newFen);
-      }
-    },
-
-    clear: function(){
-      this.update(defaultFen);
-    },
-
-    turn: function(){
-      return game.turn();
-    }
-
-  }
+  return deferred.promise;
 
 })
 
 
 .controller('AppCtrl', function($scope, Game){
 
-  $scope.$watch(Game.ready, function(isReady){
-
-    if(isReady === true){
-      $scope.fen = Game.fen();
-      $scope.turn = Game.turn;
-      $scope.$watch('fen', function(newVal){
-        Game.update(newVal);
-      });
-
-      $scope.$watch(Game.fen, function(newVal){
-        console.log('game update from server');
-        if($scope.fen !== newVal){
-          $scope.fen = newVal;
-        }
-      });
-
-    }
-
+  Game.then(function(game){
+    $scope.game = game
   });
-
 
   $scope.clear = function(){
     if (confirm('Are you sure you want to clear the board?')) {
-      Game.clear();
-      $scope.fen = Game.fen();
-      $scope.turn = Game.turn;
+      $scope.game.clear();
     }
   }
 
@@ -94,14 +62,6 @@ angular.module("MyApp", ["firebase"])
 
   var board = null;
   var game = null;
-  var dirscope = null;
-
-  var updateBoard = function(fen){
-    console.log('updating board to', fen);
-    game = new Chess(fen);
-    board.position(fen);
-  };
-
 
   // do not pick up pieces if the game is over
   // only pick up pieces for the side to move
@@ -124,7 +84,8 @@ angular.module("MyApp", ["firebase"])
     // illegal move
     if (move === null) return 'snapback';
 
-    updateStatus();
+    // update game
+    game.save();
   };
 
   // update the board position after the piece snap 
@@ -133,56 +94,16 @@ angular.module("MyApp", ["firebase"])
     board.position(game.fen());
   };
 
-  // TO REDO
-  var updateStatus = function() {
-    dirscope.$apply(function(){
-      dirscope.fen = game.fen();
-    });
-
-    // var status = '';
-
-    // var moveColor = 'White';
-    // if (game.turn() === 'b') {
-    //   moveColor = 'Black';
-    // }
-
-    // // checkmate?
-    // if (game.in_checkmate() === true) {
-    //   status = 'Game over, ' + moveColor + ' is in checkmate.';
-    // }
-
-    // // draw?
-    // else if (game.in_draw() === true) {
-    //   status = 'Game over, drawn position';
-    // }
-
-    // // game still on
-    // else {
-    //   status = moveColor + ' to move';
-
-    //   // check?
-    //   if (game.in_check() === true) {
-    //     status += ', ' + moveColor + ' is in check';
-    //   }
-    // }
-
-    // statusEl.html(status);
-    // fenEl.html(game.fen());
-    // pgnEl.html(game.pgn());
-  };
-
   return {
     restrict: 'E',
 
     scope: {
-      'fen': '=fen',
+      'game': '=game',
     },
 
     link: function(scope, element, attrs){
       dirscope = scope;
-
-      var fen = scope.fen;
-      game = new Chess(fen);
+      game = scope.game
 
       board = new ChessBoard(element, {
         position: game.fen(),
@@ -192,24 +113,12 @@ angular.module("MyApp", ["firebase"])
         onSnapEnd: onSnapEnd
       });
 
-      scope.$watch('fen', function(newPosition, oldPosition){
-        console.log('setting up board positions', newPosition, oldPosition);
-        if(newPosition !== oldPosition && newPosition !== game.fen()){
-          updateBoard(newPosition);
-        }
+
+      // Update board layout on game changes
+      scope.$watch(game.fen, function(newPos, oldPos){
+        console.log('repainting board');
+        board.position(game.fen());
       });
     }
   }
 });
-
-// function MyController($scope, $firebase) {
-//   var todosRef = new Firebase("https://torid-fire-3930.firebaseio.com/todos");
-//   // Automatically syncs everywhere in realtime
-//   $scope.todos = $firebase(todosRef);
-//   $scope.addTodo = function() {
-//     // AngularFire $add method
-//     $scope.todos.$add({name: $scope.newTodo});
-
-//     $scope.newTodo = "";
-//   }
-// }
